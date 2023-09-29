dbacf4ff889158238ee6ddc74fe2f1693ebc6327
diarisdiakite <diarisdiakite@gmail.com>
2023-09-29 04:13:39 +0200

    Fix linters.

--- my_list.rb
+++ my_list.rb
@@ -1,14 +1,14 @@
-require_relative 'MyEnumerable'
+require_relative 'my_enumerable'
 
 class MyList
   include MyEnumerable
-  
-  #create an instance variable @list
+
+  # create an instance variable @list
   def initialize(*items)
     @list = items
   end
 
-  #implement a method #each that yields successive members of @list and uses the MyEnumerable module
+  # implement a method #each that yields successive members of @list and uses the MyEnumerable module
   def call_find_all(&block)
     find_all(@list, &block)
   end
@@ -19,7 +19,3 @@ my_list = MyList.new(1, 2, 3, 6, 22)
 # Find all number greater than 2
 all_items = my_list.call_find_all { |item| item > 2 }
 puts "The list items greater than 2 are: #{all_items}"
-
-
-
-
